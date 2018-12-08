f6551b446c1c29cab8ae92d387a552008eaccb6c
jiwan21 <jiwanjk@ucla.edu>
2018-12-08 00:56:03 +0000

    add unityreceive

--- Testfiles/udpsend.py
+++ Testfiles/udpsend.py
@@ -1,8 +1,8 @@
 
 import socket
 import time
-UDP_IP = "192.168.0.19"
-UDP_PORT = 65501
+UDP_IP = "192.168.0.36"
+UDP_PORT = 65300
 MESSAGE = 'Bonjour, le monde!'
 
 print( "UDP target IP:", UDP_IP)
@@ -10,7 +10,8 @@ print( "UDP target port:", UDP_PORT)
 print( "message:", MESSAGE)
 count = 0
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.connect((UDP_IP, UDP_PORT))
+error = sock.connect_ex((UDP_IP, UDP_PORT))
+print "Error: ", error
 print("Beginning to send")
 while True:
 	sock.send(MESSAGE.encode('utf-8'))
