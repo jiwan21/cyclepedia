1d32b01371215eef83eac1197ed0867bf28b5666
jiwan2ne1 <jiwanjk@ucla.edu>
2018-12-08 01:12:00 -0800

    add imu.py

---
+++ rpi/imu.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+import sys
+import time
+import math
+import IMU
+import datetime
+import os
+# If the IMU is upside down (Skull logo facing up), change this value to 1
+IMU_UPSIDE_DOWN = 1	
+
+
+RAD_TO_DEG = 57.29578
+M_PI = 3.14159265358979323846
+G_GAIN = 0.070  	# [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+AA =  0.40      	# Complementary filter constant
+MAG_LPF_FACTOR = 0.4 	# Low pass filter constant magnetometer
+ACC_LPF_FACTOR = 0.4 	# Low pass filter constant for accelerometer
+ACC_MEDIANTABLESIZE = 9    	# Median filter table size for accelerometer. Higher = smoother but a longer delay
+MAG_MEDIANTABLESIZE = 9    	# Median filter table size for magnetometer. Higher = smoother but a longer delay
+
+
+oldXAccRawValue = 0
+oldYAccRawValue = 0
+oldZAccRawValue = 0
+
+a = datetime.datetime.now()
+
+#Setup the tables for the mdeian filter. Fill them all with '1' soe we dont get devide by zero error 
+acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2X = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2Y = [1] * ACC_MEDIANTABLESIZE
+acc_medianTable2Z = [1] * ACC_MEDIANTABLESIZE
+
+
+IMU.detectIMU()     #Detect if BerryIMUv1 or BerryIMUv2 is connected.
+IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
+
+
+while True:
+
+    #Read the accelerometer,gyroscope and magnetometer values
+    ACCx = IMU.readACCx()
+    ACCy = IMU.readACCy()
+    ACCz = IMU.readACCz()
+    GYRx = IMU.readGYRx()
+    GYRy = IMU.readGYRy()
+    GYRz = IMU.readGYRz()
+    MAGx = IMU.readMAGx()
+    MAGy = IMU.readMAGy()
+    MAGz = IMU.readMAGz()
+
+
+    #Apply compass calibration    
+    MAGx -= (magXmin + magXmax) /2 
+    MAGy -= (magYmin + magYmax) /2 
+    MAGz -= (magZmin + magZmax) /2 
+ 
+
+    ##Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds/(1000000*1.0)
+    print "Loop Time | %5.2f|" % ( LP ),
+
+    ############################################### 
+    #### Apply low pass filter ####
+    ###############################################
+    ACCx =  ACCx  * ACC_LPF_FACTOR + oldXAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCy =  ACCy  * ACC_LPF_FACTOR + oldYAccRawValue*(1 - ACC_LPF_FACTOR);
+    ACCz =  ACCz  * ACC_LPF_FACTOR + oldZAccRawValue*(1 - ACC_LPF_FACTOR);
+
+    oldXMagRawValue = MAGx
+    oldYMagRawValue = MAGy
+    oldZMagRawValue = MAGz
+    oldXAccRawValue = ACCx
+    oldYAccRawValue = ACCy
+    oldZAccRawValue = ACCz
+
+    ######################################### 
+    #### Median filter for accelerometer ####
+    #########################################
+    # cycle the table
+    for x in range (ACC_MEDIANTABLESIZE-1,0,-1 ):
+        acc_medianTable1X[x] = acc_medianTable1X[x-1]
+        acc_medianTable1Y[x] = acc_medianTable1Y[x-1]
+        acc_medianTable1Z[x] = acc_medianTable1Z[x-1]
+
+    # Insert the lates values
+    acc_medianTable1X[0] = ACCx
+    acc_medianTable1Y[0] = ACCy
+    acc_medianTable1Z[0] = ACCz    
+
+    # Copy the tables
+    acc_medianTable2X = acc_medianTable1X[:]
+    acc_medianTable2Y = acc_medianTable1Y[:]
+    acc_medianTable2Z = acc_medianTable1Z[:]
+
+    # Sort table 2
+    acc_medianTable2X.sort()
+    acc_medianTable2Y.sort()
+    acc_medianTable2Z.sort()
+
+    # The middle value is the value we are interested in
+    ACCx = acc_medianTable2X[ACC_MEDIANTABLESIZE/2];
+    ACCy = acc_medianTable2Y[ACC_MEDIANTABLESIZE/2];
+    ACCz = acc_medianTable2Z[ACC_MEDIANTABLESIZE/2];
+
+
+
+    ####################################################################
+    ###################Tilt compensated heading#########################
+    ####################################################################
+    #Normalize accelerometer raw values.
+    if not IMU_UPSIDE_DOWN:        
+        #Use these two lines when the IMU is up the right way. Skull logo is facing down
+        accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+        accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+    else:
+        #Us these four lines when the IMU is upside down. Skull logo is facing up
+        accXnorm = -ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+        accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+    #Calculate pitch and roll
+
+    pitch = math.asin(accXnorm)
+    roll = -math.asin(accYnorm/math.cos(pitch))
+    ############################ END ##################################
+
+    if 1:			#Change to '0' to stop showing the angles from the accelerometer
+        print ("#Roll: %.4f" % roll)
+
+    #slow program down a bit, makes the output more readable
+    time.sleep(0.03)
+
+
